2ba59a3e0fac3120c79a6b85c43fc6fbf1c4fa3e
strothj <jason@jasons.io>
2017-04-07 05:24:10 -0400

    feature: add database connect function

---
+++ src/server/core/database.js
@@ -0,0 +1,49 @@
+import mongoose from 'mongoose';
+
+const DEFAULT_TEST_DB = 'mongodb://localhost/nxcms-api-test';
+
+export const getTestConnectionString = () => (
+  process.env.DB_TEST || DEFAULT_TEST_DB
+);
+
+mongoose.Promise = Promise;
+
+let initialized = false;
+
+export const connect = async (connectionString) => {
+  if (process.env.NODE_ENV === 'test') {
+    // eslint-disable-next-line no-param-reassign
+    connectionString = getTestConnectionString();
+  }
+
+  if (!connectionString) throw new Error('database connection string is required');
+
+  const opts = process.env.NODE_ENV !== 'development' ? {} : {
+    server: { socketOptions: { connectTimeoutMS: 25 } },
+  };
+
+  if (initialized) return;
+
+  await mongoose.connect(connectionString, opts);
+  initialized = true;
+};
+
+export const disconnect = () => new Promise((resolve, reject) => {
+  if (!initialized) { resolve(); return; }
+
+  const resetState = () => {
+    initialized = false;
+    if (mongoose.connection.readyState !== 0 /* disconnected */) {
+      reject(new Error('mongo connection could not be disconnected')); return;
+    }
+    resolve();
+  };
+
+  // disconnect callback is called while connection is in the "disconnecting"
+  // state rather than the "disconnected" state. Only call resetState through
+  // the callback when the connection is reported to be disconnected.
+  mongoose.connection.once('disconnected', resetState);
+  mongoose.disconnect((err) => {
+    if (err) resetState();
+  });
+});
---
+++ src/server/core/database.spec.js
@@ -0,0 +1,52 @@
+import { expect } from 'chai';
+import mongoose from 'mongoose';
+import {
+  connect,
+  disconnect,
+  getTestConnectionString,
+} from './database';
+
+const disconnectedState = 0;
+const connectedState = 1;
+
+describe('database', () => {
+  before(() => {
+    disconnect();
+    expect(mongoose.connection.readyState).to.equal(disconnectedState);
+  });
+  afterEach(() => disconnect());
+
+  describe('connect', () => {
+    describe('during unit tests', () => {
+      it('gets connection string from environment', async () => {
+        await connect();
+        expect(mongoose.connection.readyState).to.equal(connectedState);
+      });
+    });
+
+    describe('outside of unit tests', () => {
+      before(() => { process.env.NODE_ENV = 'development'; });
+      after(() => { process.env.NODE_ENV = 'test'; });
+
+      it('rejects on connection failure', (done) => {
+        connect('mongodb://99.99.99.99/non-existant')
+          .catch(() => { done(); });
+      });
+
+      it('connects using provided connection string', async () => {
+        expect(mongoose.connection.readyState).to.equal(disconnectedState);
+        await connect(getTestConnectionString());
+        expect(mongoose.connection.readyState).to.equal(connectedState);
+      });
+    });
+  });
+
+  describe('disconnect', () => {
+    it('disconencts from server', async () => {
+      await connect();
+      expect(mongoose.connection.readyState).to.equal(connectedState);
+      await disconnect();
+      expect(mongoose.connection.readyState).to.equal(disconnectedState);
+    });
+  });
+});
